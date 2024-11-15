b8536db6a5f55eeb4a0591cb6629528ec9ec4202
Brian Larson <brian@brianlarson.com>
2024-11-15 16:48:06 -0600

    Finish setting up getting and rendering todos from the db, add styling based on completion status

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,53 @@
-console.log('JS is sourced!');
+// Get and render todos on load
+getTodos();
+
+// Get todos list location in DOM
+const todoListUl = document.getElementById("todoList");
+
+// GET and refresh todos from db and render to DOM
+function getTodos() {
+  // Send Axios HTTP GET request to server
+  axios({
+    method: "GET",
+    url: "/todos"
+  })
+    .then((response) => {
+      // GET request and response successful so render data to DOM
+      console.log("/todos GET request sent - response from server:", response.data);
+      renderTodos(response.data);
+  })
+    .catch((error) => {
+      // Log error
+      console.log("/todos GET request encountered an error:", error);
+  });
+}
+
+// Render todos to DOM
+function renderTodos(todos) {
+  // Clear list before re-rendering
+  todoListUl.innerHTML = "";
+
+  // Loop through todos received from database and add to DOM
+  for (const todo of todos) {
+    // Handle look of todo based on completed status
+    let liClassList = todo.isComplete ? " bg-secondary-subtle completed": "";
+    let checkedAttr = todo.isComplete ? " checked" : "";
+
+    // Append retrieved todos to list
+    todoListUl.innerHTML += `
+      <li id="todo-${todo.id}" class="list-group-item${liClassList}">
+        <div class="row g-3">
+          <div class="col-8 d-flex align-items-center">
+            <input class="form-check-input mt-0" type="checkbox"${checkedAttr}>
+            <label class="form-check-label" for="firstCheckbox">
+              <span>${todo.text}</span>
+            </label>
+          </div>
+          <div class="col-4 d-flex justify-content-end">
+            <button class="btn btn-sm btn-outline-danger">Delete</button>
+          </div>
+        </div>
+      </li>
+    `;
+  }
+}
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
-
+// Create GET router to get and return all to-dos from db
+router.get("/", (req, res) => {
+  // Set and run SQL query statement to retrieve todos
+  // in order by ID
+  let statement = `SELECT * FROM "todos" ORDER BY "id" ASC`;
+  pool.query(statement)
+    .then(result => {
+      // That worked so log it and return the db
+      // rows/todo items
+      console.log("Getting todos from db…", result.rows);
+      res.send(result.rows);
+  })
+    .catch(error => {
+      // Log error and send HTTP status
+      console.log("Error getting todos from database…", error);
+      res.sendStatus(500);
+  });
+});
 
 module.exports = router;
