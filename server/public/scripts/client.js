e5c28909bcc35bed5eaf89d7ee958e8632e64896
Brian Larson <brian@brianlarson.com>
2024-11-16 00:06:50 -0600

    Finish POST and DELETE functions for adding and deleting todos

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,13 @@
 // Get and render todos on load
 getTodos();
 
-// Get todos list location in DOM
+// Get todos list location and todo text input
 const todoListUl = document.getElementById("todoList");
+const todoTextInput = document.getElementById("todoText");
 
-// GET and refresh todos from db and render to DOM
+// Make GET HTTP request to retrieve todos from db and render them
 function getTodos() {
+
   // Send Axios HTTP GET request to server
   axios({
     method: "GET",
@@ -13,28 +15,75 @@ function getTodos() {
   })
     .then((response) => {
       // GET request and response successful so render data to DOM
-      console.log("/todos GET request sent - response from server:", response.data);
+      // console.log("/todos GET request sent - response from server:", response.data);
       renderTodos(response.data);
   })
     .catch((error) => {
       // Log error
       console.log("/todos GET request encountered an error:", error);
   });
+
 }
 
-// TODO: Create function to add new todos
+// Make POST HTTP request to add a new todo, refresh from db and re-render
+function addTodo(event) {
+
+  // Prevent button in form from refreshing page
+  event.preventDefault();
+
+  // Send Axios HTTP POST request to server
+  axios({
+    method: "POST",
+    url: "/todos",
+    data: { text: todoTextInput.value }
+  })
+    .then((response) => {
+      // GET request and response successful so get updated todos and re-render to DOM
+      // console.log("/todos GET request sent - response from server:", response.data);
+      getTodos();
+  })
+    .catch((error) => {
+      // Log error
+      console.log("/todos GET request encountered an error:", error);
+  });
+
+  // Clear the todo text input
+  todoTextInput.value = ``;
+
+}
 
 // TODO: Create function to toggle completed status on todos with PUT request
 
-// TODO: Create function to delete todos from list with DELETE request
+// Make DELETE HTTP request to delete todo, refresh from db and re-render
+function deleteTodo(todoId) {
 
-// Render todos to DOM
+  // Send Axios HTTP DELETE request to server
+  axios({
+    method: "DELETE",
+    url: "/todos",
+    data: { id: todoId }
+  })
+    .then((response) => {
+      // DELETE request and response successful so get updated todos and re-render to DOM
+      // console.log("/todos DELETE request sent - response from server:", response.data);
+      getTodos();
+  })
+    .catch((error) => {
+      // Log error
+      console.log("/todos DELETE request encountered an error:", error);
+  });
+
+}
+
+// Render todos
 function renderTodos(todos) {
+
   // Clear list before re-rendering
   todoListUl.innerHTML = "";
 
   // Loop through todos received from database and add to DOM
   for (const todo of todos) {
+
     // Handle look of todo based on completed status
     const liClassList = todo.isComplete ? ` bg-transparent fst-italic text-secondary completed`: ` bg-dark-subtle`;
     const checkedAttr = todo.isComplete ? ` checked` : ``;
@@ -42,7 +91,7 @@ function renderTodos(todos) {
 
     // Append retrieved todos to list
     todoListUl.innerHTML += `
-      <li id="todo-${todo.id}" class="list-group-item${liClassList}" data-testid="toDoItem">
+      <li class="list-group-item${liClassList}" data-testid="toDoItem">
         <div class="row g-3">
           <div class="col-8 d-flex align-items-center">
             <input class="form-check-input mt-0" type="checkbox" data-testid="completeButton"${checkedAttr}>
@@ -52,10 +101,11 @@ function renderTodos(todos) {
             </label>
           </div>
           <div class="col-4 d-flex justify-content-end">
-            <button onClick="deleteTodo(event, ${todo.id})" class="btn btn-sm btn-outline-danger" data-testid="deleteButton">Delete</button>
+            <button onclick="deleteTodo(${todo.id})" class="btn btn-sm btn-outline-danger" data-testid="deleteButton">Delete</button>
           </div>
         </div>
       </li>
     `;
+
   }
 }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,29 +1,76 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-// Create GET router to get and return all to-dos from db
+// ********************
+// ! 🙀 Oh C.R.U.D.!
+// ********************
+
+// ! CREATE
+// POST - add a new todo to the db
+router.post("/", (req, res) => {
+
+  // Run SQL query statement to add new todo to db with pg.Pool
+  // TODO Figure out why $1 replacement for SQL injection security doesn't work
+  // * $1, $2 etc. used in template for security against SQL injection
+  // * Also, note that sequel statements require terminator at end ";" or an
+  // * error will occur
+  const statement = `INSERT INTO "todos"("text") VALUES ('${req.body.text}');`;
+  pool.query(statement)
+    // That worked so log it and return HTTP created status
+    .then(result => {
+      console.log("Adding todo to db…");
+      res.sendStatus(201);
+  })
+    .catch(error => {
+      // Log error and send HTTP error status
+      console.log("Error adding todo to database…", error);
+      res.sendStatus(500);
+  });
+
+});
+
+// ! READ
+// GET - get and return all todos from db
 router.get("/", (req, res) => {
-  // Set and run SQL query statement to retrieve todos
-  // in order by ID
+
+  // Run SQL query statement with pg.Pool to retrieve todos
+  // ordered by ID
   let statement = `SELECT * FROM "todos" ORDER BY "id" ASC`;
   pool.query(statement)
     .then(result => {
-      // That worked so log it and return the db
-      // rows/todo items
-      console.log("Getting todos from db…", result.rows);
+      // That worked so log it and return the db rows/todo items
+      // console.log("Getting todos from db…", result.rows);
       res.send(result.rows);
   })
     .catch(error => {
-      // Log error and send HTTP status
+      // Log error and send HTTP error status
       console.log("Error getting todos from database…", error);
       res.sendStatus(500);
   });
+
 });
 
-// TODO: Set up POST router for adding todos
-
+// ! UPDATE
 // TODO: Set up PUT router for changing `isComplete` status
+// PUT - update a todo from the db
 
-// TODO: Set up DELETE router for removing todos from db
+// ! DELETE
+// DELETE - delete a todo from the db
+router.delete("/", (req, res) => {
+
+  // Run SQL query statement with pg.Pool to delete todo by ID
+  let statement = `DELETE FROM "todos" WHERE "id" = ${req.body.id}`;
+  pool.query(statement)
+   .then(result => {
+    // That worked so return an OK status
+    res.sendStatus(200);
+ })
+   .catch(error => {
+     // Log error and send HTTP error status
+     console.log("Error getting todos from database…", error);
+     res.sendStatus(500);
+ });
+
+});
 
 module.exports = router;
