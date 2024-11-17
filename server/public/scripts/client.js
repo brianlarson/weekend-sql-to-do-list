1bbf497e3523f5d09da1dd80888e8f172fcd35c7
Brian Larson <brian@brianlarson.com>
2024-11-16 19:41:49 -0600

    Update checkbox input to be a button and style it, tweak completed badge

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -55,20 +55,6 @@ function addTodo(event) {
 // TODO: Create function to toggle completed status on todos with PUT request
 // Make PUT HTTP request to update todo completed status, refresh from db and re-render
 function toggleStatus(event, todoId) {
-  console.log(`toggleStatus() was called…`);
-
-  // Get whether or not todo is checked off (completed)
-  const checkboxInput = event.target;
-  console.log(checkboxInput);
-
-  let isChecked = checkboxInput.getAttribute("data-ischecked");
-  console.log(`isChecked:`, isChecked);
-
-  // if (isChecked) {
-  //   checkboxInput.setAttribute("data-ischecked", true);
-  // }
-
-  // console.log({ id: todoId, isComplete: isChecked });
 
   // Send Axios HTTP PUT request to server with todo to update
   // axios({
@@ -122,18 +108,22 @@ function renderTodos(todos) {
     const liClassList = todo.isComplete ? ` bg-transparent fst-italic text-secondary completed`: ` bg-dark-subtle`;
     const checkedAttr = todo.isComplete ? ` checked` : ``;
     const isChecked = todo.isComplete ? true : false;
-    const badgeHtml = todo.isComplete ? `<span class="badge rounded-pill text-bg-success fst-normal fw-light">Completed</span>` : ``;
+    const badgeHtml = todo.isComplete ? `<span class="badge rounded-pill text-bg-success fst-normal fw-light">Completed</span>` : `
+    <span class="badge rounded-pill bg-transparent text-success border border-success fst-normal fw-light">Completed</span>
+    `;
 
     // Append retrieved todos to list
     todoListUl.innerHTML += `
       <li class="list-group-item${liClassList}" data-testid="toDoItem">
         <div class="row g-3">
           <div class="col-8 d-flex align-items-center">
-            <input onchange="toggleStatus(event, ${todo.id})" class="form-check-input mt-0" type="checkbox" data-ischecked="${isChecked}" data-testid="completeButton"${checkedAttr}>
-            <label class="form-check-label" for="firstCheckbox">
-              <span>${todo.text}</span>
-              ${badgeHtml}
-            </label>
+            <button id="completeBtn" onclick="toggleStatus(event, ${todo.id})" class="btn btn-outline-secondary" data-testid="completeButton">
+              <svg class="d-none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="800" height="800" viewBox="0 0 17.837 17.837"><path d="M16.145 2.571a.7.7 0 0 0-.99 0L6.92 10.804l-4.241-4.27a.698.698 0 0 0-.989 0L.204 8.019a.703.703 0 0 0 0 .99l6.217 6.258a.704.704 0 0 0 .99 0L17.63 5.047a.7.7 0 0 0 0-.994l-1.485-1.482z"/></svg>
+            </button>
+            <div class="d-flex align-items-center ms-3">
+              <div>${todo.text}</div>
+              <div class="ms-3">${badgeHtml}</div>
+            </div>
           </div>
           <div class="col-4 d-flex justify-content-end">
             <button onclick="deleteTodo(${todo.id});" class="btn btn-sm btn-outline-danger" data-testid="deleteButton">Delete</button>
